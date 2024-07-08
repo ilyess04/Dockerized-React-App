9f2bef4ce57cc630e0f69ededcd14d398edac1e7
Sellamingo <ilyessellami04@gmail.com>
2024-07-08 17:02:29 +0100

    Add and Configure docker files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
